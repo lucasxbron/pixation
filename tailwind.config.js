16d5b9344e8e16c174fdb16b2e7eecb8eb19d733
Lucas <lucasbron@mailbox.org>
2024-10-04 13:22:03 +0200

    feat: tailwind dependencies, config / header styling / variables

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    extend: {colors: {
+      primary: 'rgb(var(--color-primary))',
+      secondary: 'rgb(var(--color-secondary))',
+      background: 'rgb(var(--color-background))',
+      btn: 'rgb(var(--color-btn), 0.153)',
+      btnhover: 'rgb(var(--color-btn), 0.291)',
+    }, 
+    fontFamily: {
+      'serif': ['Pacifico', 'system-ui'],
+    }},
+  },
+  plugins: [],
+};
