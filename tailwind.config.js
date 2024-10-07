14fe5ab27b8f43b6d2743381085b67a92454ddb1
Lucas <lucasbron@mailbox.org>
2024-10-07 09:11:59 +0200

    feat: header responsivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {colors: {
-      primary: 'rgb(var(--color-primary))',
-      secondary: 'rgb(var(--color-secondary))',
-      background: 'rgb(var(--color-background))',
-      btn: 'rgb(var(--color-btn), 0.153)',
-      btnhover: 'rgb(var(--color-btn), 0.291)',
-    }, 
-    fontFamily: {
-      'serif': ['Pacifico', 'system-ui'],
-    }},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary))",
+        secondary: "rgb(var(--color-secondary))",
+        background: "rgb(var(--color-background))",
+        btn: "rgb(var(--color-btn), 0.153)",
+        btnhover: "rgb(var(--color-btn), 0.291)",
+      },
+      fontFamily: {
+        serif: ["Pacifico", "system-ui"],
+      },
+      gridTemplateColumns: {
+        3: "1fr 4fr 7fr",
+      },
+      // gridTemplateRows: {
+      //   '3': '1fr 4fr 7fr',
+      // }
+    },
   },
   plugins: [],
 };
